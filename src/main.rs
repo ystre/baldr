47d25320778f87bdc3cc8baaa3b7a100d0c0307c
Chris <gk.project72@gmail.com>
2024-09-28 22:58:08 +0200

    Feat: Running built binary (#2)
    Additional features:
    * Running under debugger
    * Configuration file support
    * Clean build
    * Rebuilding with CMake no-configure

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
-pub mod cli;
-pub mod cmake;
-pub mod utils;
-
-use crate::utils::debug_cmd;
-use crate::cli::{Commands, ExeArgs};
-
 use clap::Parser;
+use config::Config;
+
+use baldr::{
+    Args,
+    build,
+    configure,
+    find_files,
+    format_cmd,
+    read_config,
+    read_input,
+};
 
 use log::*;
 
 use std::{
-    error::Error,
     io,
-    os::unix::fs,
+    fs,
+    fmt,
+    os::unix::fs::symlink,
     path::{
+        self,
         Path,
         PathBuf
     },
     process::{
+        self,
         Command,
         ExitStatus
     }
@@ -40,15 +47,18 @@ impl<'a> BuildPath<'a> {
     /// - sanitizers (if used)
     ///
     fn to_path(&self) -> PathBuf {
-        let compiler = Path::new(&self.compiler_path).file_name().unwrap();
+        let compiler = match self.compiler_path {
+            "" => String::new(),
+            _  => format!("-{}", Path::new(&self.compiler_path).file_name().expect("Invalid compiler path").to_string_lossy()),
+        };
 
         let dir = format!(
-            "{}-{}{}",
+            "{}{}{}",
             self.build_type.to_lowercase(),
-            compiler.to_str().unwrap(),
+            compiler,
             match self.sanitizer {
-                Some(san) => format!("-{}", san),
-                None => "".into()
+                Some(san) => format!("-{san}"),
+                None => String::new()
             }
         );
 
@@ -58,82 +68,156 @@ impl<'a> BuildPath<'a> {
     }
 }
 
-impl std::fmt::Display for BuildPath<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl fmt::Display for BuildPath<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_fmt(format_args!("{}", self.to_path().display()))
     }
 }
 
-fn create_compile_cmd_symlink(src: &PathBuf, dst: &PathBuf) -> Result<(), io::Error> {
+fn create_compile_cmd_symlink(src: &Path, dst: &Path) -> Result<(), io::Error> {
     let file = "compile_commands.json";
-    if std::fs::exists(dst.join(file)).is_err() {
-        return fs::symlink(src.join(file), dst.join(file));
+    let src = path::absolute(src.join(file))?;
+    let dst = dst.join(file);
+
+    match fs::exists(&dst) {
+        Ok(true) => {
+            debug!("`compile_commands.json` symlink already exists and is valid.");
+            Ok(())
+        }
+        Ok(false) => {
+            match fs::remove_file(&dst) {
+                Ok(()) => { debug!("Broken `compile_commands.json` symlink is removed."); },
+                Err(e) => { debug!("`compile_commands.json` symlink cannot be removed: {e}"); },
+            };
+
+            debug!("Creating `compile_commands.json` symlink...");
+            symlink(src, &dst)
+        }
+        Err(e) => Err(e),
     }
-    Ok(())
 }
 
-fn build(args: &cli::Args, build_dir: &PathBuf) -> Result<(), Box<dyn Error>> {
-    cmake::configure(&build_dir, &args)?;
-    cmake::build(&build_dir, &args)?;
-    create_compile_cmd_symlink(&build_dir, &args.project.clone().into())?;
+fn delete_build_dir(build_dir: &Path, confirm: bool) -> Result<bool, String> {
+    if confirm {
+        eprint!("Are you sure to remove `{}` (press 'y' to proceed): ", build_dir.to_string_lossy());
 
-    Ok(())
+        if read_input() != "y" {
+            info!("Skipping clean build.");
+            return Ok(false);
+        }
+
+    }
+    else {
+        info!("Non-interactive mode, skipping confirmation for deleting build directory.");
+    }
+
+    fs::remove_dir_all(build_dir).map_err(|e| format!("Failed to delete build directory: {e}"))?;
+    info!("Build directory deleted!");
+    Ok(true)
 }
 
-fn run(target: &String, build_dir: &PathBuf, args: &ExeArgs) -> Result<ExitStatus, Box<dyn Error>> {
+fn run(target: &String, build_dir: &PathBuf, config: &Config, args: &Args) -> Result<ExitStatus, String> {
     if target == "all" {
         return Err("Target must be specified".into());
     }
 
-    let exes = utils::find_files(build_dir, |filename| { filename == *target });
+    let exes = find_files(build_dir, |filename| { filename == *target });
     match exes.len() {
         1 => {
-            let mut cmd = Command::new(&exes[0]);
-            cmd.args(&args.args);
-            let mut process = cmd.spawn().expect("Failed to run the built executable!");
-            debug_cmd(&cmd);
+            let mut cmd = (|| -> Result<Command, String> {
+                if args.debug {
+                    let debugger = config.get::<String>("debugger")
+                        .map_err(|e| format!("No debugger is configured: {e}"))?;
 
-            Ok(process.wait()?)
+                    let mut cmd = Command::new(&debugger);
+                    if debugger == "gdb" {
+                        cmd.arg("--args");
+                        cmd.arg(&exes[0]);
+                    } else if debugger == "lldb" {
+                        cmd.arg(&exes[0]);
+                    }
+                    Ok(cmd)
+                } else {
+                    Ok(Command::new(&exes[0]))
+                }
+            })()?;
+
+            cmd.args(&args.exe_args);
+            let mut process = cmd.spawn().map_err(|e| format!("Failed to run the built executable: {e}"))?;
+            let cmd_str = format_cmd(&cmd);
+            Ok(process.wait().map_err(|e| format!("Command `{cmd_str}` did not start; {e}"))?)
+
         },
-        0 => Err(format!("No executable found in `{}`", build_dir.display()).into()),
-        _ => Err(format!("Multiple executables found in `{}`", build_dir.display()).into()),
+        0 => Err(format!("No executable found in `{}`", build_dir.display())),
+        _ => Err(format!("Multiple executables found in `{}`", build_dir.display())),
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    env_logger::builder()
-        .format_timestamp_millis()
-        .init();
-
-    let args = cli::Args::parse();
+fn entrypoint() -> Result<(), String> {
+    let args = Args::parse();
+    let config = read_config(&args.config).map_err(|e| e.to_string())?;
 
     let build_dir = BuildPath{
         project: args.project.as_str(),
         build_type: args.build_type.as_str(),
-        // compiler_path: compiler.1.as_str(),
-        compiler_path: "na",      // TODO(feat): configurable compilers
+        compiler_path: &config.get_string("compiler.cxx").unwrap_or_default(),
         sanitizer: None
     }.to_path();
 
-    match std::fs::exists(&build_dir) {
-        Ok(_) => info!("Build directory already exists at: {}", build_dir.to_string_lossy()),
-        Err(_) => {
-            std::fs::create_dir_all(&build_dir)?;
-            info!("Build directory created at: {}", build_dir.to_string_lossy());
+    info!("Using build directory: {}", build_dir.to_string_lossy());
+
+    let mut build_exists = match fs::exists(&build_dir) {
+        Ok(true) => {
+            info!("Build directory already exists.");
+            Ok(true)
+        },
+        Ok(false) => Ok(false),
+        Err(x) => Err(format!("{x}")),
+    }?;
+
+    if args.delete {
+        if build_exists {
+            build_exists = !delete_build_dir(&build_dir, !args.no_confirm)?;
+        } else {
+            warn!("Build directory does not exist, there is nothing to delete!");
+        }
+
+        if !build_exists {
+            fs::create_dir_all(&build_dir).map_err(|e| format!("Failed to create build directory: {e}"))?;
+            info!("Build directory has been created.");
         }
     }
 
-    match &args.command {
-        Commands::Build{} => {
-            build(&args, &build_dir)?;
-        }
-        Commands::Run(exe_args) => {
-            build(&args, &build_dir)?;
-            run(&args.target, &build_dir, exe_args)?;
-        }
+    if !build_exists || !args.no_configure {
+        configure(build_dir.as_path(), &args, &config)?;
+    }
+
+    if !build(build_dir.as_path(), &args)?.success() {
+        return Err("Build failed".into());
+    }
+
+    create_compile_cmd_symlink(build_dir.as_path(), Path::new(&args.project))
+        .map_err(|e| format!("Failed to create a symlink for `compile_commands.json`: {e}"))?;
+
+    if args.run {
+        run(&args.target, &build_dir, &config, &args)?;
     }
 
     Ok(())
+}
+
+fn main() {
+    env_logger::builder()
+        .format_timestamp_millis()
+        .init();
+
+    match entrypoint() {
+        Ok(()) => {},
+        Err(e) => {
+            log::error!("Fatal error encountered: {e}");
+            process::exit(1);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -141,7 +225,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn build_dir() {
+    fn build_dir_default() {
+        assert_eq!(
+            BuildPath{
+                project: "project".into(),
+                build_type: "Debug".into(),
+                compiler_path: "".into(),
+                sanitizer: None
+            }.to_path().to_string_lossy(),
+            "project/build/debug"
+        );
+    }
+
+    #[test]
+    fn build_dir_sanitizer() {
+        assert_eq!(
+            BuildPath{
+                project: "project".into(),
+                build_type: "Debug".into(),
+                compiler_path: "".into(),
+                sanitizer: Some("asan")
+            }.to_path().to_string_lossy(),
+            "project/build/debug-asan"
+        );
+    }
+
+    #[test]
+    fn build_dir_compiler() {
         assert_eq!(
             BuildPath{
                 project: "project".into(),
@@ -151,13 +261,16 @@ mod tests {
             }.to_path().to_string_lossy(),
             "project/build/debug-gcc"
         );
+    }
 
+    #[test]
+    fn build_dir_both() {
         assert_eq!(
             BuildPath{
                 project: "project".into(),
                 build_type: "Debug".into(),
                 compiler_path: "gcc".into(),
-                sanitizer: Some("asan".into())
+                sanitizer: Some("asan")
             }.to_path().to_string_lossy(),
             "project/build/debug-gcc-asan"
         );
